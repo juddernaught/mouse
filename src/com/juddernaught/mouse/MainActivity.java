7319edce07436e59c0ecb138bc65afb45d79492d
Daniel Judd <juddernaught@gmail.com>
2013-04-06 21:54:53 -0400

    Workable camera

--- src/com/juddernaught/mouse/MainActivity.java
+++ src/com/juddernaught/mouse/MainActivity.java
@@ -4,19 +4,83 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "CameraDemo";
+	Camera camera;
+	Preview preview;
+	Button buttonClick;
+
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		preview = new Preview(this);
+		((FrameLayout) findViewById(R.id.preview)).addView(preview);
+
+		buttonClick = (Button) findViewById(R.id.buttonClick);
+		buttonClick.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				preview.camera.takePicture(shutterCallback, rawCallback,
+						jpegCallback);
+			}
+		});
+
+		Log.d(TAG, "onCreate'd");
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	ShutterCallback shutterCallback = new ShutterCallback() {
+		public void onShutter() {
+			Log.d(TAG, "onShutter'd");
+		}
+	};
+
+	/** Handles data for raw picture */
+	PictureCallback rawCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Log.d(TAG, "onPictureTaken - raw");
+		}
+	};
+
+	/** Handles data for jpeg picture */
+	PictureCallback jpegCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			FileOutputStream outStream = null;
+			try {
+				// write to local sandbox file system
+				// outStream =
+				// CameraDemo.this.openFileOutput(String.format("%d.jpg",
+				// System.currentTimeMillis()), 0);
+				// Or write to sdcard
+				outStream = new FileOutputStream(String.format(
+						"/sdcard/poop%d.jpg", System.currentTimeMillis()));
+				outStream.write(data);
+				outStream.close();
+				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+			}
+			Log.d(TAG, "onPictureTaken - jpeg");
+		}
+	};
 
 }
